2301b092ab6c61fd27b18fa887ae13bad5729d22
TM <tjmcard@gmail.com>
2024-01-04 08:07:50 -0800

    ItemLocalDataSource implementation

--- app/src/main/java/com/abyxcz/mad_room/data/DefaultDataSource.kt
+++ app/src/main/java/com/abyxcz/mad_room/data/DefaultDataSource.kt
@@ -3,19 +3,32 @@ package com.abyxcz.mad_room.data
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Main entry point for accessing tasks data.
+ * Main entry point for accessing items data.
  */
 interface DefaultDataSource {
 
+
+    fun observeItems(): Flow<Result<List<ItemEntity>>>
+
     suspend fun getItems(): Result<List<ItemEntity>>
 
-    suspend fun getItem(taskId: String): Result<ItemEntity>
+    suspend fun refreshItems()
 
-    suspend fun saveItem(task: ItemEntity)
+    fun observeItem(itemId: String): Flow<Result<ItemEntity>>
 
-    suspend fun updateItem(task: ItemEntity) : Int
+    suspend fun getItem(itemId: String): Result<ItemEntity>
 
-    suspend fun deleteItem(task: ItemEntity)
+    suspend fun refreshItem(itemId: String)
 
-    suspend fun deleteAllItems()
+    suspend fun saveItem(item: ItemEntity)
+
+    suspend fun saveItems(items: List<ItemEntity>)
+
+    suspend fun updateItem(item: ItemEntity) : Int
+
+    suspend fun updateItems(items: List<ItemEntity>) : Int
+
+    suspend fun clearAllItems()
+
+    suspend fun clearItem(itemId: String)
 }
--- app/src/main/java/com/abyxcz/mad_room/data/ItemDao.kt
+++ app/src/main/java/com/abyxcz/mad_room/data/ItemDao.kt
@@ -4,21 +4,95 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object for the items table.
+ */
 @Dao
 interface ItemDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addUsers(users: List<ItemEntity>)
+        /**
+         * Observes list of items.
+         *
+         * @return all items.
+         */
+        @Query("SELECT * FROM Items")
+        fun observeItems(): Flow<List<ItemEntity>>
 
-    @Query("SELECT * FROM items")
-    fun getAllUsers(): List<ItemEntity>
+        /**
+         * Observes a single item.
+         *
+         * @param itemId the item id.
+         * @return the item with itemId.
+         */
+        @Query("SELECT * FROM Items WHERE id = :itemId")
+        fun observeItemById(itemId: String): Flow<ItemEntity>
 
-    @Query("SELECT * FROM items WHERE itemId = :userId")
-    fun getUser(userId: Int): Flow<ItemEntity>
+        /**
+         * Select all items from the items table.
+         *
+         * @return all items.
+         */
+        @Query("SELECT * FROM Items")
+        suspend fun getItems(): List<ItemEntity>
 
-    @Query("DELETE FROM items")
-    suspend fun deleteAllUsers()
+        /**
+         * Select a item by id.
+         *
+         * @param itemId the item id.
+         * @return the item with itemId.
+         */
+        @Query("SELECT * FROM Items WHERE id = :itemId")
+        suspend fun getItemById(itemId: String): ItemEntity?
 
-}
+        /**
+         * Insert a item in the database. If the item already exists, replace it.
+         *
+         * @param itemEntity the item to be inserted.
+         */
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        suspend fun insertItem(itemEntity: ItemEntity)
+
+        /**
+         * Insert a list of items in the database. If the item already exists, replace it.
+         *
+         * @param itemEntity the item to be inserted.
+         */
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        suspend fun insertItems(itemEntities: List<ItemEntity>)
+
+        /**
+         * Update a item.
+         *
+         * @param itemEntity item to be updated
+         * @return the number of items updated. This should always be 1.
+         */
+        @Update
+        suspend fun updateItem(itemEntity: ItemEntity): Int
+
+        /**
+         * Update a list of items.
+         *
+         * @param itemEntity item to be updated
+         * @return the number of items updated. This should always be 1.
+         */
+        @Update
+        suspend fun updateItems(itemEntities: List<ItemEntity>): Int
+
+        /**
+         * Delete a item by id.
+         *
+         * @return the number of items deleted. This should always be 1.
+         */
+        @Query("DELETE FROM Items WHERE id = :itemId")
+        suspend fun deleteItemById(itemId: String): Int
+
+        /**
+         * Delete all items.
+         */
+        @Query("DELETE FROM Items")
+        suspend fun deleteItems()
+
+    }
